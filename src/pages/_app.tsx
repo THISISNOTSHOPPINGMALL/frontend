9cc2c927e13edfbc4a3bdd0967eabefc99d123bc
nana <byyulli16@gmail.com>
2022-07-17 17:22:04 +0900

    update setting for build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
 import type { AppProps } from 'next/app';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+
+import createEmotionCache from '../utility/createEmotionCache';
 import '../styles/global.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component ponent {...pageProps} />;
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
+
+const clientSideEmotionCache = createEmotionCache();
+
+import '../styles/global.css';
+
+const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </CacheProvider>
+  );
+};
 
 export default MyApp;
